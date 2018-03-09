4cbcc153ad42c337b1cc2a73fde727ca83827ad9
Alan Trick <me@alantrick.ca>
2018-03-08 16:52:03 -0800

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(filename):
 
 setup(
     name='django-courier',
-    version='0.5.0',
+    version='0.5.1',
     description='A notification app for Django',
     long_description=read('README.rst'),
     author='Alan Trick',
